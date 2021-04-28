221972915506e35bd2c209ce6b9c00e21d1f42db
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-04-27 20:25:32 -0700

    formatting ronnie coleman reply

--- somebot.py
+++ somebot.py
@@ -1,57 +1,57 @@
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
 import os
-import praw
+# import praw
+import asyncpraw
 import asyncio
 import aioredis
 
 from redis_conn import connect
 
-asyncio.run(connect())
-
+# asyncio.run(connect())
 load_dotenv()
-REPLY_RONNIE = "_You have summoned the Donger's archnemesis **RONNIE COLEMAN** \n https://generationiron.com/wp-content/uploads/2019/12/Ronnie-Coleman-Reveals-Who-Handed-Him-His-Most-Bitter-Loss.jpg_"
-# CLIENT_ID = os.environ.get("CLIENT_ID")
-# print(os.environ.get("CLIENT_SECRET"))
-# print(os.environ.get("PASSWORD"))
-# print(os.environ.get("CLIENT_USERNAME"))
-# print(CLIENT_ID)
 
-reddit = praw.Reddit(
-    client_id=os.environ.get("CLIENT_ID"),
-    client_secret=os.environ.get("CLIENT_SECRET"),
-    password=os.environ.get("PASSWORD"),
-    user_agent="bot for r/admiralbulldog by u/sonareads",
-    username=os.environ.get("CLIENT_USERNAME"),
-)
+async def main():
+  redis = await aioredis.create_redis_pool('redis://localhost')
 
-# print(reddit.user.me())
+  emotes = await redis.hgetall("emotes", encoding="utf-8")
+  # print(emotes);
 
-reddit.read_only = False
+  redis.close()
+  await redis.wait_closed()
 
-# some_string = "Ronnie coleman"
-# some_string = "ronnie coleman"
-# some_string = "ronnie Coleman"
-# some_string = "asdfasdfasdfronnie Colemanasdasdasd"
+  REPLY_RONNIE = """_You have summoned the great Donger's archnemesis **RONNIE COLEMAN**_
+  
+  https://generationiron.com/wp-content/uploads/2019/12/Ronnie-Coleman-Reveals-Who-Handed-Him-His-Most-Bitter-Loss.jpg_
+  
+  ###### From Just another Reddit Bot."""
 
-# if "ronnie coleman" in some_string.lower():
-#   print("yes")
+  reddit = asyncpraw.Reddit(
+      client_id=os.environ.get("CLIENT_ID"),
+      client_secret=os.environ.get("CLIENT_SECRET"),
+      password=os.environ.get("PASSWORD"),
+      user_agent="bot for r/admiralbulldog by u/sonareads",
+      username=os.environ.get("CLIENT_USERNAME"),
+  )
 
-# url_title = quote_plus("asdfasdfasdfronnie Colemanasdasdasd")
-# reply_text = REPLY_TEMPLATE.format(url_title);
-# print(reply_text);
+  # print(await reddit.user.me())
 
-# for submission in reddit.subreddit("AdmiralBulldog").stream.submissions():
-for submission in reddit.subreddit("TestBotAdmiral").stream.submissions():
-  if "ronnie coleman" in submission.title:
+  reddit.read_only = False
 
-    # url_title = quote_plus(submission.title)
-    # reply_text = REPLY_TEMPLATE.format(url_title);
-    # print(reply_text);
-    submission.reply(REPLY_RONNIE)
-  # for top_level_comment in submission.comments:
-    # if "ronnie coleman" in top_level_comment.body:
-      # url_title = quote_plus(top_level_comment.body)
-      # reply_text = REPLY_TEMPLATE.format(url_title);
-      # print(reply_text);
-      # top_level_comment.reply(reply_text)
+  subreddit = await reddit.subreddit("TestBotAdmiral")
+  # submissions = await admiral_subreddit.stream.submissions()
+
+  async for submission in subreddit.stream.submissions():
+    if "ronnie coleman" in submission.title:
+      print("submission found")
+      await submission.reply(REPLY_RONNIE)
+    # for top_level_comment in submission.comments:
+      # if "ronnie coleman" in top_level_comment.body:
+        # top_level_comment.reply(reply_text)
+
+# asyncio.run(main())
+
+if __name__ == "__main__":
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main())
+
