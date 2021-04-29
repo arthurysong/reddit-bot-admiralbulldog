650473e5d441e63ed60c3fe18c6c02c0a0b4c663
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-04-29 11:40:50 -0700

    testwords.py => parse_string a modular function to be used in somebot.py

---
+++ parse_string.py
@@ -0,0 +1,34 @@
+import string
+
+# test emotes
+emotes = {
+  "WTFF": '/WTFF',
+  "LUL": '/LUL',
+  "KEKL": '/KEKL',
+  "KEKW": 'KEKW',
+  "MonkaS": '/MonkaS'
+}
+
+test_string = "LUL LUL LUL LUL LUL LUL LULL"
+
+def parse_string(comment, emotes):
+  # accept a string, and emote dictionary
+  # return the list of emotes found
+
+  # any punctuation is replaced with white spaces
+  # if duplicate emote is present only one should be returned in dictionary
+
+  trimmed_str = comment.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation)))
+
+  emote_present = {};
+
+  for word in trimmed_str.split(" "):
+    if word in emotes.keys() and not word in emote_present.keys():
+      emote_present[word] = True
+      # print(word, ":", emotes[word])
+
+  return list(emote_present)
+
+
+# print(parse_string(test_string, emotes))
+# print(list(emotes))
