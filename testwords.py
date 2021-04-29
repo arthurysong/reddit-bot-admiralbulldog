116a7b57bf52fe5931cc4f7b8033c8b0b401937a
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-04-28 18:24:11 -0700

    working on controlling for punctuation

--- testwords.py
+++ testwords.py
@@ -1,22 +1,31 @@
 # emotes = ["WTFF", "LUL", "KEKL", "KEKW"]
+import string
 
 emotes = {
   "WTFF": '/WTFF',
   "LUL": '/LUL',
   "KEKL": '/KEKL',
   "KEKW": 'KEKW',
+  "MonkaS": '/MonkaS'
 }
 
 # test_string = "bulldog pudge LUL KEKW WTFF"
 # any punctuation should be removed....
-test_string = "bulldog pudge LUL! LUL"
+test_string = "MonkaS...KEKL"
+
+# multiple emotes should be only linked once...
+# test_string = "LUL LUL LUL LUL LUL"
 
 # if any(word in test_string for word in emotes_):
 
 # for word in emotes:
 #   if (word in test_string):
 #     print(word);
+test_string = test_string.translate(str.maketrans(' ', ' ', string.punctuation))
+print(test_string)
+emote_present = {};
 
 for word in test_string.split(" "):
-  if word in emotes.keys():
+  if word in emotes.keys() and not word in emote_present.keys():
+    emote_present[word] = True
     print(word, ":", emotes[word])
