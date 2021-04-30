14fc4e3fe4434a48df56ff2518da7fe329ac9183
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-04-29 18:39:45 -0700

    add args to stream so it only gets NEW comments, submissions :)

--- main.py
+++ main.py
@@ -23,71 +23,72 @@ async def monitor_submissions_for_ronnie():
   """this process will monitor only submissions and reply with an image of RONNIE COLEMAN. does not 
   monitor any comments"""
 
-  try: 
-    reddit = asyncpraw.Reddit(
-        client_id=os.environ.get("CLIENT_ID"),
-        client_secret=os.environ.get("CLIENT_SECRET"),
-        password=os.environ.get("PASSWORD"),
-        user_agent="bot for r/admiralbulldog by u/sonareads",
-        username=os.environ.get("CLIENT_USERNAME"),
-    )
+  reddit = asyncpraw.Reddit(
+      client_id=os.environ.get("CLIENT_ID"),
+      client_secret=os.environ.get("CLIENT_SECRET"),
+      password=os.environ.get("PASSWORD"),
+      user_agent="bot for r/admiralbulldog by u/sonareads",
+      username=os.environ.get("CLIENT_USERNAME"),
+  )
 
-    reddit.read_only = False
+  reddit.read_only = False
 
-    subreddit = await reddit.subreddit(SUBREDDIT, fetch=True)
+  subreddit = await reddit.subreddit(SUBREDDIT, fetch=True)
 
-    async for submission in subreddit.stream.submissions():
-      # TODO ronnie coleman title should be normalized?
-      # TODO should control for punctuation
-      if "ronnie coleman" in submission.title:
-        print("submission found")
-        await submission.reply(REPLY_RONNIE)
-  except RequestException:
-    print("there was an error connecting with praw")
-    monitor_submissions_for_ronnie
+  async for submission in subreddit.stream.submissions(skip_existing=True):
+    # TODO ronnie coleman title should be normalized?
+    # TODO should control for punctuation
+    if "ronnie coleman" in submission.title:
+      print("submission found")
+      await submission.reply(REPLY_RONNIE)
 
 
 async def monitor_comments_for_bttv_emotes():
   """this process will monitor the comments streams for any comments containing bttv emotes
   and reply with image urls"""
 
-  try:
-    emotes = await get_emotes();
+  emotes = await get_emotes();
 
-    reddit = asyncpraw.Reddit(
-        client_id=os.environ.get("CLIENT_ID"),
-        client_secret=os.environ.get("CLIENT_SECRET"),
-        password=os.environ.get("PASSWORD"),
-        user_agent="bot for r/admiralbulldog by u/sonareads",
-        username=os.environ.get("CLIENT_USERNAME"),
-    )
+  reddit = asyncpraw.Reddit(
+      client_id=os.environ.get("CLIENT_ID"),
+      client_secret=os.environ.get("CLIENT_SECRET"),
+      password=os.environ.get("PASSWORD"),
+      user_agent="bot for r/admiralbulldog by u/sonareads",
+      username=os.environ.get("CLIENT_USERNAME"),
+  )
 
-    bot_account = await reddit.user.me();
+  bot_account = await reddit.user.me();
 
-    reddit.read_only = False
+  reddit.read_only = False
 
-    subreddit = await reddit.subreddit(SUBREDDIT, fetch=True)
+  subreddit = await reddit.subreddit(SUBREDDIT, fetch=True)
 
-    async for comment in subreddit.stream.comments():
-      if (comment.author == bot_account): 
-        print("don't reply to comment from self")
-        continue
+  async for comment in subreddit.stream.comments(skip_existing=True):
+    if (comment.author == bot_account): 
+      print("don't reply to comment from self")
+      continue
 
-      reply = "_Ah! A Twitch emote user: no doubt a man of exquisite culture and refined tastes. :3_"
-      print("comment", comment.body)
+    reply = "_Ah! A Twitch emote user: no doubt a man of exquisite culture and refined tastes. :3_"
+    print("comment", comment.body)
 
-      emotes_found = parse_string(comment.body, emotes)
+    emotes_found = parse_string(comment.body, emotes)
 
-      if (emotes_found):
-        for emote in emotes_found:
-          reply += f'\n\n{emote}: https://cdn.betterttv.net/emote/{emotes[emote]}/3x'
+    if (emotes_found):
+      for emote in emotes_found:
+        reply += f'\n\n{emote}: https://cdn.betterttv.net/emote/{emotes[emote]}/3x'
 
-        print("emote found")
-        reply += "\n\n ###### From Just another Reddit Bot."
-        await comment.reply(reply)
-  except RequestException:
-    print("there was an error connecting with praw")
-    monitor_submissions_for_ronnie
+      print("emote found")
+      reply += "\n\n ###### From Just another Reddit Bot."
+      await comment.reply(reply)
+  # except:
+  #   print("there was an error connecting with praw")
+  #   monitor_comments_for_bttv_emotes()
+
+# async def handle_exception(awaitable):
+#   try:
+#     return await awaitable
+#   except:
+#     print("exception was raised...");
 
 
 if __name__ == "__main__":
