91d911eb19557a0679db3a20071609b061b3c297
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-05-03 13:21:37 -0700

    use deployed redis

--- redis_func.py
+++ redis_func.py
@@ -1,8 +1,13 @@
 import requests_async as requests
 import aioredis
 import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 URL = "https://api.betterttv.net/3/emotes/shared/top?offset=0&limit=100"
+REDIS = os.environ.get("CLIENT_ID")
 
 async def update_emotes():
   """queries bttv api for top 100 global emotes and sets them in redis db"""
@@ -15,7 +20,7 @@ async def update_emotes():
   for emote in data:
     dictionary[emote["emote"]["code"]] = emote["emote"]["id"]
 
-  redis = await aioredis.create_redis_pool('redis://localhost')
+  redis = await aioredis.create_redis_pool(REDIS)
 
   # TIL hmset_dict will UPDATE the emotes key with new key values in dictionary
   await redis.hmset_dict("emotes", dictionary)
@@ -26,7 +31,7 @@ async def update_emotes():
 async def delete_emotes():
   """deletes emotes in redis db"""
   
-  redis = await aioredis.create_redis_pool('redis://localhost')
+  redis = await aioredis.create_redis_pool(REDIS)
   await redis.delete("emotes")
 
   redis.close()
@@ -35,7 +40,7 @@ async def delete_emotes():
 async def get_emotes():
   """fetches emotes from redis db"""
 
-  redis = await aioredis.create_redis_pool('redis://localhost')
+  redis = await aioredis.create_redis_pool(REDIS)
   value = await redis.hgetall("emotes", encoding="utf-8")
 
   redis.close()
@@ -47,7 +52,7 @@ async def test_update_emotes():
 
   dictionary = { "LUL": "/LUL" }
 
-  redis = await aioredis.create_redis_pool('redis://localhost')
+  redis = await aioredis.create_redis_pool(REDIS)
   await redis.hmset_dict("test_emotes", dictionary)
 
   redis.close()
