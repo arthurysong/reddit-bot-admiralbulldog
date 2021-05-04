1c2654ce559664f8ec03f1470a140ea8e8301080
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-05-03 18:23:45 -0700

    shell script will run python script and output sdout to output.txt

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ ssh -i '~/.ssh/arthurec2.pem' ubuntu@13.57.195.73 /bin/bash <<EOT
   cd reddit-bot-admiralbulldog
   git pull 
   kill -9 $BOT_ID
-  python3 main.py & # run process in background
+  python3 -u main.py > output.txt &
   BOT_ID=$! # set BOT_ID equal to process running
 EOT
