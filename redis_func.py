27ff331a916ba20ce9515f16aae6248bc05e6de5
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-05-06 14:57:48 -0700

    update emotes daily will fetch ALL emotes from BTTV api not just top 100

--- redis_func.py
+++ redis_func.py
@@ -3,14 +3,48 @@ import aioredis
 import asyncio
 from dotenv import load_dotenv
 import os
+from pprint import pprint
 
 load_dotenv()
 
 URL = "https://api.betterttv.net/3/emotes/shared/top?offset=0&limit=100"
 REDIS = os.environ.get("REDIS_URL") if os.environ.get("APP_ENV") != "dev" else "redis://localhost"
 
+async def update_all_emotes():
+  """figure out how to get all 10000 emotes from bttv"""
+
+  offset = 0
+  while True:
+    r = await requests.get(url = f'https://api.betterttv.net/3/emotes/shared/top?offset={offset}&limit=100')
+    data = r.json()
+    # try to get 100 emotes
+
+    # pprint(data);
+    # print(len(data))
+
+    if (len(data) == 0):
+      break;
+    
+    offset += len(data) 
+
+    dictionary = {};
+
+    for emote in data:
+      dictionary[emote["emote"]["code"]] = emote["emote"]["id"]
+
+    redis = await aioredis.create_redis_pool(REDIS)
+
+    # TIL hmset_dict will UPDATE the emotes key with new key values in dictionary
+    await redis.hmset_dict("emotes", dictionary)
+
+    redis.close()
+    await redis.wait_closed()
+
+
 async def update_emotes():
   """queries bttv api for top 100 global emotes and sets them in redis db"""
+
+
 
 
   print(REDIS)
@@ -52,7 +86,7 @@ async def get_emotes():
 async def test_update_emotes():
   """A test function to update the hash set for a key"""
 
-  dictionary = { "LUL": "/LUL" }
+  dictionary = { "Boo": "/Boo" }
 
   redis = await aioredis.create_redis_pool(REDIS)
   await redis.hmset_dict("test_emotes", dictionary)
@@ -63,6 +97,6 @@ async def test_update_emotes():
 async def update_emotes_daily_process():
   while 1:
     print("updating emotes....")
-    await update_emotes()
+    await update_all_emotes()
     print("updated emotes!")
     await asyncio.sleep(86400);
