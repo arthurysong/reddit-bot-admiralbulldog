d26990ed1d8102794b82db96d691f68532b3be55
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-05-07 17:30:13 -0700

    update script should be runnable in prod

--- update.sh
+++ update.sh
@@ -3,13 +3,20 @@
 # this script will pull changes in ec2 instance and restart bot with changes.
 
 # this doesn't work yet
-ssh -i '~/.ssh/arthurec2.pem' ubuntu@13.57.195.73 /bin/bash <<EOT
-  # update and restart should
-  cd reddit-bot-admiralbulldog
-  git pull 
-  echo "test"
-  echo $BOT_ID
-  kill -9 $BOT_ID
-  python3 -u main.py >> output.txt &
-  BOT_ID=$! # set BOT_ID equal to process running
-EOT
+# ssh -i '~/.ssh/arthurec2.pem' ubuntu@13.57.195.73 /bin/bash <<EOT
+#   # update and restart should
+#   cd reddit-bot-admiralbulldog
+#   git pull 
+#   echo "test"
+#   echo $BOT_ID
+#   kill -9 $BOT_ID
+#   python3 -u main.py >> output.txt &
+#   BOT_ID=$! # set BOT_ID equal to process running
+# EOT
+
+git pull 
+echo "test"
+echo $BOT_ID
+kill -9 $BOT_ID
+python3 -u main.py >> output.txt &
+BOT_ID=$! # set BOT_ID equal to process running
