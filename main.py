369bc23b31543fb22a4683b6f0774052f90244b2
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-05-10 17:33:10 -0700

    two utility functions to create markdown from sadge tuple in conf

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
 #!/usr/bin/env python3
 
 from dotenv import load_dotenv
-from urllib.parse import quote_plus
-from parse_string import parse_string, check_string_for_ronnie, check_string_for_sadge
+from string_utils import check_string_for_ronnie, check_string_for_sadge
+from conf import SADGE_RESPONSES, SUBREDDIT, SIGNATURE, REPLY_RONNIE
 import os
-# import praw
 import asyncpraw
 import asyncio
-# import aioredis
 import asyncprawcore
 import logging
-import aioschedule as schedule
-import sys, traceback
-
-from redis_func import update_emotes_daily_process, get_bttv_emotes, get_ff_emotes
-from concurrent.futures import ProcessPoolExecutor
+import random
 
 load_dotenv()
-
-# SUBREDDIT = "TestBotAdmiral" if os.environ.get("APP_ENV") == "dev" else "AdmiralBulldog"
-SUBREDDIT = "TestBotAdmiral"
-SIGNATURE = """---
-> It's always a good time to be Sadge
-^(From Just another Sadge Reddit Bot)
-
-[*^(Issues & Suggestions)*](https://github.com/arthurysong/reddit-bot-admiralbulldog/issues) *^(|)* 
-[*^(Source)*](https://github.com/arthurysong/reddit-bot-admiralbulldog) *^(|)* 
-[*^(Creator)*](https://www.reddit.com/user/Sonareads)
-"""
-REPLY_RONNIE = """_You have summoned the great Donger's archnemesis [***Ronnie Coleman***](https://generationiron.com/wp-content/uploads/2019/12/Ronnie-Coleman-Reveals-Who-Handed-Him-His-Most-Bitter-Loss.jpg)_
-
-%s""" % (SIGNATURE)
-
-print(SUBREDDIT)
-
 
 async def monitor_submissions_for_ronnie():
   """this process will monitor only submissions and reply with an image of RONNIE COLEMAN. does not 
@@ -63,22 +40,12 @@ async def monitor_submissions_for_ronnie():
       await asyncio.sleep(1)
 
 
-async def monitor_comments_for_bttv_emotes():
+async def monitor_comments_for_sadge():
   """this process will monitor the comments streams for any comments containing bttv emotes
   and reply with image urls"""
 
   while 1:
     try: 
-      # try: 
-      #   bttv_emotes = await get_bttv_emotes();
-      #   ff_emotes = await get_ff_emotes();
-      # except Exception as inst:
-      #   # if can't connect to redis just shut off for now...
-      #   print(type(inst))
-      #   print(inst.args)
-      #   print("could not connect to redis")
-      #   sys.exit(3)
-
       reddit = asyncpraw.Reddit(
           client_id=os.environ.get("CLIENT_ID"),
           client_secret=os.environ.get("CLIENT_SECRET"),
@@ -100,55 +67,27 @@ async def monitor_comments_for_bttv_emotes():
           print("don't reply to comment from self")
           continue
 
-        # the comment body should just match an emote EXACTLY
-        # not contain the emote because I don't want to spam the subreddit
-
-        # bttv_emote = emote_match(comment.body, bttv_emotes)
         reply = ""
-
-        # 
 
         if check_string_for_sadge(comment.body):
           print("sadge found!")
           sadge_ff_id = "472535" # emote id for Sadge in frankerfacez api
           emote_size = "4" # avail size is 1, 2, 4
-          reply += f'\n\n[Sadge](https://cdn.frankerfacez.com/emote/{sadge_ff_id}/{emote_size})'
+          reply += random.choice(SADGE_RESPONSES)[0]
+          reply += f'\n\n> It\'s always a good time to be [Sadge](https://cdn.frankerfacez.com/emote/{sadge_ff_id}/{emote_size})'
           reply += f'\n\n {SIGNATURE}'
           await comment.reply(reply)
 
-        # reply = "_Ah! A Twitch emote user: no doubt a man of exquisite culture and refined tastes. 🍷🍷🍷_"
-        # print("comment", comment.body)
-
-        # check for bttv emotes
-
-        # bttv_emotes_found = parse_string(comment.body, bttv_emotes)
-        # ff_emotes_found = parse_string(comment.body, ff_emotes)
-
-        # if (bttv_emotes_found or ff_emotes_found):
-        #   for emote in bttv_emotes_found:
-        #     emote_size = '3x' # avail size is 1x, 2x, 3x
-        #     reply += f'\n\n* [{emote}](https://cdn.betterttv.net/emote/{bttv_emotes[emote]}/{emote_size})'
-
-        #   for emote in ff_emotes_found:
-        #     emote_size = '4' # avail size is 1, 2, 4
-        #     reply += f'\n\n* [{emote}](https://cdn.frankerfacez.com/emote/{ff_emotes[emote]}/{emote_size})'
-
-        #   print("emote found")
-        #   reply += f'\n\n {SIGNATURE}'
-        #   await comment.reply(reply)
     except asyncprawcore.exceptions.RequestException:
       logging.warning("emote process couldn't connect to praw.. restarting process")
       await asyncio.sleep(1)
 
 async def run_tasks():
   ronnie_process = asyncio.create_task(monitor_submissions_for_ronnie())
-  bttv_process = asyncio.create_task(monitor_comments_for_bttv_emotes())
-  # update_emotes_daily = asyncio.create_task(update_emotes_daily_process())
-
+  bttv_process = asyncio.create_task(monitor_comments_for_sadge())
 
   await ronnie_process
   await bttv_process
-  # await update_emotes_daily
 
 if __name__ == "__main__":
   # TIL if python file is executed as script then __name__ == __main__
--- string_utils.py
+++ string_utils.py
@@ -13,6 +13,7 @@ emotes = {
 
 test_string = "Sadge.. you will be remembered"
 
+# DEL don't think this is being used anymore .. 
 def parse_string(comment, emotes):
   """Check if string contains any emotes in Redis db"""
 
@@ -41,6 +42,17 @@ def check_string_for_sadge(comment):
   
   return False
 
+def decorate_sadge(quote):
+  """Accept a quote that contains exactly 'Sadge' and decorate the word Sadge with bold and link to Sadge emote"""
+  sadge_ff_id = "472535" # emote id for Sadge in frankerfacez api
+  emote_size = "4" # avail size is 1, 2, 4
+  return quote.replace("Sadge", f'[**Sadge**](https://cdn.frankerfacez.com/emote/{sadge_ff_id}/{emote_size})', 1)
+
+def markdown_from_sadge_tuple(tuple):
+  """From a Sadge tuple in conf.py return the markdown to be used as a comment"""
+
+  return f'> {decorate_sadge(tuple[0])} \n\n - {tuple[1]}'
+
 def check_string_for_ronnie(text):
   "check if string contains the substring 'ronnie coleman'"
 
@@ -57,3 +69,5 @@ def check_string_for_ronnie(text):
 # print(parse_string("monkaS LUL", emotes))
 
 # print(check_string_for_sadge("....sadg..."))
+# print(decorate_sadge("Sometimes it takes noise to appreciate silence, absence to value presence, and Sadge to know happiness."))
+# print(markdown_from_sadge_tuple(("Sometimes it takes noise to appreciate silence, absence to value presence, and Sadge to know happiness.", "Unknown")))
